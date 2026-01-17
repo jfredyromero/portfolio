ccfc092179dde0fd8cae9de5a4e8e7517774347a
Jhon Romero <jhon.romero@digitashealth.com>
2026-01-16 20:57:03 -0500

    Deployment to Github Pages configured

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,8 @@ import tailwindcss from "@tailwindcss/vite";
 // https://astro.build/config
 export default defineConfig({
 	integrations: [react()],
-
+	site: "https://jfredyromero.github.io",
+	base: "/portfolio",
 	vite: {
 		ssr: {
 			noExternal: ["gsap"],
